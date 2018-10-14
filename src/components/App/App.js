826ed4410ff0819b2d4200d59532ea218e2b6838
Balbuhhha <balbuhhha@gmail.com>
2018-10-14 12:07:56 +0200

    default contact names appear in the list

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,38 +2,51 @@ import React, { Component } from 'react';
 import './App.css';
 
 
-const contacts = [
-  {
-    id: 1,
-    name: 'Barry',
-    surname: 'Fletcher',
-    number: '43243443'
-  },
-  {
-    id: 2,
-    name: 'Harry',
-    surname: 'Smith',
-    number: '76555543'
-  },
-  {
-    id: 3,
-    name: 'Marry',
-    surname: 'Butcher',
-    number: '97653234'
-  }
-]
+
 class App extends Component {
-  
+
   state = {
-    contacts: []
+    contacts: [
+      {
+        id: 1,
+        name: 'Barry',
+        surname: 'Fletcher',
+        number: '43243443'
+      },
+      {
+        id: 2,
+        name: 'Harry',
+        surname: 'Smith',
+        number: '76555543'
+      },
+      {
+        id: 3,
+        name: 'Marry',
+        surname: 'Butcher',
+        number: '97653234'
+      }
+    ]
   }
-  
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1>Contact List</h1>
         </header>
+        <main className="App-main">
+          <ul>
+            {
+              this.state.contacts.map(
+                contact => (
+                  <li key={contact.id}>
+                    {contact.name}
+                  </li>
+                )
+              )
+            }
+          </ul>
+        </main>
       </div>
     );
   }
